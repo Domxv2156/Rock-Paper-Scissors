b274da84b1c8f78eefa070a9aeb450d95aa22fc5
Dominik Kloc <dominic2156@hotmail.com>
2022-07-16 11:40:16 +0100

    set up one round of game

--- java.js
+++ java.js
@@ -11,6 +11,26 @@ function playerChoice() {
     if(input === "rock" && getComputerChoice() === "paper") {
 alert('You lost!');
     }
+    else if(input === "paper" && getComputerChoice() === "scissors") {
+        alert('You lost!'); }
+        else if(input === "scissors" && getComputerChoice() === "rock") {
+            alert('you lost!');
+        } else if(input === "rock" && getComputerChoice() === "rock") {
+            alert('tie!');
+        } else if(input === "scissors" && getComputerChoice() === "scissors") {
+            alert('tie!'); }
+            else if(input === "paper" && getComputerChoice() === "paper") {
+                alert('tie!');
+             } else if(input === "paper" && getComputerChoice() === "rock") {
+                alert('You won!'); }
+                else if(input === "rock" && getComputerChoice() === "scissors") {
+                    alert('You won!'); }
+                    else if(input === "scissors" && getComputerChoice() === "paper") {
+                        alert('You won!'); }
+                        else {
+                           alert('Enter either rock, paper or scissors.');
+                        }
+               
     
 }
 
